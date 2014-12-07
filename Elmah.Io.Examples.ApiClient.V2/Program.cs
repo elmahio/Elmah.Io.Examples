732c2bd3209252747e0efddf770c2649f84628ed
Thomas Ardal <thomasardal@gmail.com>
2014-12-07 09:34:29 +0100

    usings cleanup

--- Elmah.Io.Examples.ApiClient.V2/Program.cs
+++ Elmah.Io.Examples.ApiClient.V2/Program.cs
@@ -1,11 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Elmah.Io.Examples.ApiClient.V2
 {
